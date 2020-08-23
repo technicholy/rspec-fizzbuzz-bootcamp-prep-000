5d701d20a200341943be0f6410073aefd03d0365
Clayton Pierce <bamer78@gmail.com>
2020-08-23 22:48:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
-  if n / 3 == 1
+  if n % 3 == 0
     return "Fizz"
-  elsif n / 5 == 1
+  elsif n % 5 == 0
     return "Buzz"
-  elsif n / 15
+  elsif n % 15 == 0
     return "FizzBuzz"
   end
 end
